7f20ca45d8d1dd64d683d7e50689d9f94eb9365e
Luna Rico <lunarico93@gmail.com>
2023-08-04 23:18:12 -0300

    Sliders Mobile

--- js/properties.js
+++ js/properties.js
@@ -834,14 +834,11 @@ let swiper;
         slidesPerView: 1,
         spaceBetween: 10,
         loop: true,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
         navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
         },
+
       });
   
       function adjustMiniaturesPosition() {
